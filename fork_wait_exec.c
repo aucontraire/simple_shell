c1b1d1f44f5a2dc8b367ee5ce07afd93059fff46
Lizzie Turner <hello@lizzieturner.com>
2018-03-21 00:58:19 +0000

    Create fork_wait_exec.c

---
+++ fork_wait_exec.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * fork_wait_exec - executes commands by first forking a child process
+ * then executing in that child while the parent process waits
+ *
+ * @commands: array of strings
+ * commands[0] is command to execute
+ * remaining strings are arguments to use with that command
+ */
+
+void fork_wait_exec(char **commands)
+{
+	pid_t pid;
+	int status;
+
+	status = 0;
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork failure");
+		_exit(1);
+	}
+
+	else if (pid == 0)
+	{
+		if ((execvp(commands[0], commands)) < 0)
+		{
+			perror(commands[0]);
+			_exit(1);
+		}
+		_exit(0);
+
+	}
+
+	wait(&status);
+}
