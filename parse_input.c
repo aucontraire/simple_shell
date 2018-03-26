0e954d357ff6fb43a4c9f06d50e0903d9808ad97
Omar <ocontreras.sf@gmail.com>
2018-03-26 20:30:31 +0000

    Use strdup in parse_input

--- parse_input.c
+++ parse_input.c
@@ -12,7 +12,7 @@ char **parse_input(char *user_input, char **path_array, char *NAME)
 {
 	char **commands;
 	char *token, *dir_path;
-	int args, i, length, start;
+	int args, i, start;
 
 	args = 1;
 	i = 0;
@@ -29,32 +29,30 @@ char **parse_input(char *user_input, char **path_array, char *NAME)
 		i++;
 	}
 
-	commands = malloc(sizeof(char *) * args + 1);
+	commands = malloc(sizeof(char *) * (args + 1));
 	token = strtok(user_input, "\n ");
 
 	if (path_check(token) == -1)
 	{
 		dir_path = find_path(path_array, token);
 		if (dir_path != NULL)
-			token = dir_path;
+		{
+			commands[0] = _strdup(dir_path);
+			free(dir_path);
+		}
 		else
 		{
 			perror(NAME);
 			return (NULL);
 		}
 	}
-
-	length = _strlen(token);
-	commands[0] = malloc(sizeof(char) * length);
-	commands[0] = token;
-	token = strtok(NULL, "\n ");
+	else
+		commands[0] = _strdup(token);
 
 	for (i = 1; i < args; i++)
 	{
-		length = _strlen(token);
-		commands[i] = malloc(sizeof(char) * length);
-		commands[i] = token;
 		token = strtok(NULL, "\n ");
+		commands[i] = _strdup(token);
 	}
 	commands[i] = NULL;
 
