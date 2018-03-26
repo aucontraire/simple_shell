27cc8e8839f6c546fff112573663b21c06b62306
Omar <ocontreras.sf@gmail.com>
2018-03-26 18:18:30 +0000

    Initialize the uninitialized variables

--- shell.c
+++ shell.c
@@ -11,14 +11,17 @@
 
 int main(__attribute__((unused)) int argc, char **argv, char **env)
 {
-	char *user_input, *NAME;
+	char *user_input;
 	size_t nbytes;
 	ssize_t bytes_read;
 	char **commands, **path_array;
+	static char *NAME;
 
 	NAME = argv[0];
-
+	user_input = NULL;
 	nbytes = 0;
+	commands = NULL;
+	path_array = NULL;
 	while (1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
@@ -42,7 +45,6 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 		}
 
 		path_array = get_path_array(env);
-		/* check for NULL path_array */
 		commands = parse_input(user_input, path_array);
 		fork_wait_exec(commands, env);
 
