87f59611fbfe9a7ce6b69594be3a7b5eb657bda7
Lizzie Turner <hello@lizzieturner.com>
2018-03-28 20:14:43 +0000

    Fix betty error

--- shell.c
+++ shell.c
@@ -11,46 +11,37 @@
 
 int main(__attribute__((unused)) int argc, char **argv, char **env)
 {
-	char *user_input;
+	char *user_input = NULL;
+	char **commands = NULL;
+	char **path_array = NULL;
 	size_t nbytes = 0;
 	ssize_t bytes_read = 0;
 	char **commands, **path_array;
 	char *NAME = argv[0];
 	int atty_is = isatty(0);
 
-	user_input = NULL;
-	commands = NULL;
-	path_array = NULL;
-
 	signal(SIGINT, SIG_IGN);
-
 	while (1)
 	{
 		if (atty_is)
 			write(STDOUT_FILENO, "hella_shell$ ", 13);
-
 		bytes_read = getline(&user_input, &nbytes, stdin);
 		if (bytes_read == -1)
 		{
 			write(STDOUT_FILENO, "\n", 1);
 			exit(2);
 		}
-
 		if (exit_check(user_input) == -1)
 			continue;
-
 		if (blank_check(user_input) == 1)
 			continue;
-
 		if (env_check(user_input) == 1)
 		{
 			print_env(env);
 			continue;
 		}
-
 		path_array = get_path_array(env);
 		commands = parse_input(user_input, path_array, NAME, atty_is);
-
 		if (commands != NULL)
 		{
 			fork_wait_exec(commands, path_array, env, NAME, user_input);
@@ -58,6 +49,5 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 			free_array(path_array);
 		}
 	}
-
 	return (0);
 }
