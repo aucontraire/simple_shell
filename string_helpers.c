6a42aaaf3780b3a0e24a9cbbd1e162569bb19fa7
Lizzie Turner <hello@lizzieturner.com>
2018-03-28 23:34:05 +0000

    Add _putchar

--- string_helpers.c
+++ string_helpers.c
@@ -107,3 +107,41 @@ int _atoi(char *s)
 
 	return (num);
 }
+
+/**
+ * print_number - print an integer
+ * @n: integer to print
+ */
+
+void print_number(int n)
+{
+	int digit, sum, last;
+	int divis = 1000000000;
+	unsigned int num;
+
+	num = n;
+	sum = 0;
+
+	while (divis > 1)
+	{
+		digit = (num / divis) % 10;
+		sum += digit;
+		if (sum != 0)
+			_putchar(digit + '0');
+		divis = divis / 10;
+	}
+
+	last = num % 10;
+	_putchar(last + '0');
+}
+
+/**
+* _putchar - prints a single character to stdout
+* @c: The character to print
+*
+* Return: 1 if successful, -1 otherwise
+*/
+int _putchar(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
